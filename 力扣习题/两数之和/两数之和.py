8594211d4e23da0383843d58cfa1f25b22a2bd88
lollipopnougat <ab2defg145@gmail.com>
2019-06-27 21:35:41 +0800

    add leetcode tests

---
+++ 力扣习题/两数之和/两数之和.py
@@ -0,0 +1,50 @@
+# -*- coding=utf-8 -*-
+# 两数之和
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if target-nums[i]==nums[j]:
+                    return [i,j]
+                else:
+                    return [-1,-1]
+'''
+
+
+# 我的原解答：平均 6000ms
+class Solutiono:
+    def twoSum(self, nums: list, target: int) -> list:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                #print("nums[%d] = %d,num[%d] = %d" %(i,nums[i],j,nums[j]))
+                if (target - nums[i]) == nums[j]:
+                    return [i, j]
+        return [-1, -1]
+
+
+#  后期优化： 平均1000ms
+class Solution:
+    def twoSum(self, nums: list, target: int) -> list:
+        for i in range(len(nums)):
+            j = target - nums[i]
+            if j in nums[i + 1:]:
+                return [i, i + 1 + nums[i + 1:].index(j)]
+                #print("nums[%d] = %d,num[%d] = %d" %(i,nums[i],j,nums[j]))
+        return [-1, -1]
+
+
+# leetcode上的大佬：
+class Solution2:
+    def twoSum(self, nums: list, target: int) -> list:
+        map = {}
+        for i, j in enumerate(nums):
+            if j in map and i != map[j][1]:
+                return map[j][1], i
+            else:
+                map[target - j] = (j, i)
+
+
+s = Solution()
+print(s.twoSum([3,2,4], 6))
+input()
--- 力扣习题/回文数/回文数.py
+++ 力扣习题/回文数/回文数.py
@@ -11,17 +11,38 @@ class Solution:
         else:
             return False
 '''
+# 我的想法是将数字转字符串，反转以后再转换成整型比较，一致就是回文
 
-def isis(x: int) -> bool:
+def isPalindrome1(x: int) -> bool:
     if x >= 0:
         if x == int(str(x)[::-1]):
             return True
         else:
             return False
-    else:
+    else: # 负数肯定不是回文数
         return False
+
+#  优化：
+def isPalindrome2(x: int) -> bool:
+    s = str(x)
+    return s == s[::-1]
 
 
 while (True):
     y = int(input("请输入数字: "))
-    print(isis(y))
+    print(isPalindrome2(y))
+
+# 思考：如果不用字符串怎么办？
+
+'''
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0 or (x%10==0 and x!=0): #个位是0本身不是0的肯定不是回文数
+            return False
+        ret=0
+        while(ret<x):
+            ret=ret*10+x%10
+            x=x//10
+        return x==ret or x==ret//10
+'''
+# 通过除10和取余能把原数的反转数求出来
