a2932bd912757ad0e6fcd0d017f0a0652313e88a
lollipopnougat <ab2defg145@gmail.com>
2020-02-14 20:31:40 +0800

    提交新题目

---
+++ 动态规划/最长公共子序列/inplement.py
@@ -0,0 +1,55 @@
+class value:
+    array_b = None
+    array_c = None
+
+    def __init__(self, m, n):
+        self.array_b = [[0 for i in range(n)] for j in range(m)]
+        self.array_c = [[0 for i in range(n)] for j in range(m)]
+
+
+def lcs_len(list_x, list_y) -> value:
+    m = len(list_x)
+    n = len(list_y)
+    res = value(len(list_x), len(list_y))
+    #res.array_c = [[0 for i in range(n)] for j in range(m)]
+    for i in range(1, m):
+        for j in range(1, n):
+            if list_x[i] == list_y[j]:
+                res.array_c[i][j] = res.array_c[i - 1][j - 1] + 1
+                res.array_b[i][j] = 1
+            elif res.array_c[i - 1][j] >= res.array_c[i][j - 1]:
+                res.array_c[i][j] = res.array_c[i - 1][j]
+                res.array_b[i][j] = 2
+            else:
+                res.array_c[i][j] = res.array_c[i][j - 1]
+                res.array_b[i][j] = 3
+    return res
+
+
+def lcs_r(i, j, list_x, res: value):
+    if i == 0 or j == 0:
+        return
+    if res.array_b[i][j] == 1:
+        lcs_r(i - 1, j - 1, list_x, res)
+        print(list_x[i])
+    elif res.array_b[i][j] == 2:
+        lcs_r(i - 1, j, list_x, res)
+    else:
+        lcs_r(i, j - 1, list_x, res)
+
+
+def get_lcs(list_x, list_y):
+    list_x.insert(0, ' ')
+    list_y.insert(0, ' ')
+    m = len(list_x) - 1
+    n = len(list_y) - 1
+    tmp = lcs_len(list_x, list_y)
+    lcs_r(m, n, list_x, tmp)
+    del (list_x[0])
+    del (list_y[0])
+
+
+l1 = ['A', 'B', 'C', 'B', 'D', 'A', 'B']
+l2 = ['B', 'D', 'C', 'A', 'B', 'A']
+
+get_lcs(l1, l2)
