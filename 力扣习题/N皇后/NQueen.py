f7bbf44b3234bd71648dd8fda479eea1306c3e1a
lollipopnougat <ab2defg145@gmail.com>
2020-02-19 13:41:21 +0800

    更新力扣习题-N皇后

---
+++ 力扣习题/N皇后/NQueen.py
@@ -0,0 +1,44 @@
+class Solution:
+    num = 0
+    chess = None
+    res = []
+
+    def cal_row(self, ch: list, row: int):
+        if row == self.num:
+            tmp = []
+            for i in range(self.num):
+                str_tmp = ''.join('Q' if ch[i][j] == 1 else '.' for j in range(self.num))
+                tmp.append(str_tmp)
+            self.res.append(tmp)
+            return
+        chess_tmp = ch
+        for i in range(self.num):
+            for j in range(self.num):
+                chess_tmp[row][j] = 0
+            chess_tmp[row][i] = 1
+            if self.is_ok(chess_tmp, row, i):
+                self.cal_row(chess_tmp, row + 1)
+
+    def is_ok(self, ch: list, row: int, col: int):
+        step = 1
+        while row - step >= 0:
+            if ch[row - step][col] == 1:
+                return False
+            if col - step >= 0 and ch[row - step][col - step] == 1:
+                return False
+            if col + step < self.num and ch[row - step][col + step] == 1:
+                return False
+            step += 1
+        return True
+
+    def solveNQueens(self, n: int):
+        self.res = []
+        self.chess = [[0] * n for i in range(n)]
+        self.num = n
+        self.cal_row(self.chess, 0)
+        return self.res
+
+
+s = Solution()
+
+print(s.solveNQueens(4))
